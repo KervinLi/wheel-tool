14c168e3c596744825f34a18d40c8a985cd397f9
KervinLi <614957650@qq.com>
2021-10-11 22:54:01 +0800

    包名变更

--- src/main/java/com/tool/utils/HttpUtils.java
+++ src/main/java/com/tool/utils/HttpUtils.java
@@ -1,4 +1,4 @@
-package com.tool.http;
+package com.tool.utils;
 
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
@@ -14,14 +14,22 @@ import org.apache.http.util.EntityUtils;
 import java.nio.charset.Charset;
 
 /**
- * @Description: http 工具
+ * @Description: http 工具类
  * @Author KerVinLi
  * @Version 1.0
  */
 @Slf4j
 public class HttpUtils {
-
-    public <T> T requestStart(String requestJson, String requestUrl, String title, Class<T> tClass) {
+    /**
+     * http请求
+     *
+     * @param requestJson 请求报文Json字符串
+     * @param requestUrl 请求地址
+     * @param title 请求标题 日志跟踪
+     * @param tClass 相应报文 实体
+     * @return
+     */
+    public static <T> T requestPost(String requestJson, String requestUrl, String title, Class<T> tClass) {
         long startTime = System.currentTimeMillis();
         T resp = null;
 
@@ -34,12 +42,6 @@ public class HttpUtils {
         post.setHeader("Content-type", "application/json;charset=utf-8");
         post.setHeader("Connection", "Close");
         post.setConfig(config);
-
-        if (requestJson.length() < 3000000) {
-            log.info("{}请求报文:{}", title, requestJson);
-        } else {
-            log.info("{}请求报文太大，不宜展示");
-        }
 
         // 构建消息实体
         StringEntity entity = new StringEntity(requestJson, Charset.forName("UTF-8"));
@@ -58,20 +60,15 @@ public class HttpUtils {
                 log.info("{} 返回报文: {}", title, responseJson);
             } else {
                 log.info("{}请求异常，异常信息：{}", title, JSON.toJSONString(response));
-                //throw new RuntimeException(CommonUtil.formatString("请求异常,异常返回码:{}", response.getStatusLine().getStatusCode()));
+                throw new RuntimeException(StringUtils.formatString("请求异常,异常返回码:{}", response.getStatusLine().getStatusCode()));
             }
         } catch (Exception e) {
             log.info("{}请求出错，出错信息:{}", title, e.getMessage());
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         } finally {
-            if (requestJson.length() < 3000000) {
-                log.info("{}处理完毕 耗时:{}ms \n请求报文:{} \n返回报文:{}", title, (System.currentTimeMillis() - startTime), requestJson, JSON.toJSONString(resp));
-            } else {
-                log.info("{}处理完毕 耗时:{}ms \n请求报文过大，不宜展示 \n返回报文:{}", title, (System.currentTimeMillis() - startTime), JSON.toJSONString(resp));
-            }
+            log.info("{}处理完毕 耗时:{}ms \n请求报文:{} \n返回报文:{}", title, (System.currentTimeMillis() - startTime), requestJson, JSON.toJSONString(resp));
         }
         return resp;
     }
-
 }
