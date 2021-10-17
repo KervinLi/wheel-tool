fbee3fa0e40acd8a75cba4b16a48b4c58255e477
KervinLi <614957650@qq.com>
2021-10-17 19:22:28 +0800

    Spring Boot application integration Redis

--- src/main/java/com/tool/module/redis/RedisService.java
+++
@@ -1,4 +0,0 @@
-package com.tool.module.redis;
-
-public interface RedisService {
-}
---
+++ src/main/java/com/tool/module/redis/config/RedisConfig.java
@@ -0,0 +1,47 @@
+package com.tool.module.redis.config;
+
+import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @Description: Redis全局配置
+ * @Author KerVinLi
+ * @Version 1.0
+ */
+@Configuration
+public class RedisConfig {
+    protected static Logger logger= LoggerFactory.getLogger(RedisConfig.class);
+
+    @Bean
+    @ConditionalOnMissingBean(name = {"redisTemplate"})
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(factory);
+        //Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        //ObjectMapper om = new ObjectMapper();
+        //om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        //om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        //jackson2JsonRedisSerializer.setObjectMapper(om);
+        GenericFastJsonRedisSerializer fastJsonRedisSerializer = new GenericFastJsonRedisSerializer();
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        // key采用String的序列化方式
+        template.setKeySerializer(stringRedisSerializer);
+        // hash的key也采用String的序列化方式
+        template.setHashKeySerializer(stringRedisSerializer);
+        // value序列化方式采用fastJson
+        template.setValueSerializer(fastJsonRedisSerializer);
+        // hash的value序列化方式采用fastJson
+        template.setHashValueSerializer(fastJsonRedisSerializer);
+        template.afterPropertiesSet();
+        logger.info("Redis configuration is complete !");
+        return template;
+    }
+
+}
