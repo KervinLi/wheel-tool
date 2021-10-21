9ca4d5d6f08e6141b2d254804f2c2d6d0c0d54bc
KervinLi <614957650@qq.com>
2021-10-21 22:51:45 +0800

    Thread pool centralized configuration processing.

---
+++ src/main/java/com/tool/thread/config/ThreadPoolConfig.java
@@ -0,0 +1,63 @@
+package com.tool.thread.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+
+import java.util.concurrent.*;
+
+/**
+ * @Description:线程池统一配置
+ * @since 2021/10/21
+ */
+@Configuration
+public class ThreadPoolConfig {
+
+    private static int a = Runtime.getRuntime().availableProcessors();
+    /**
+     * 此线程池用于CPU密集型任务多线程调度
+     * CPU密集型：核心线程数=CPU核心数(或 核心线程数=CPU核心数+1)
+     * 请直接使用，使用完毕后请勿关闭线程池
+     * @return
+     */
+    @Primary
+    @Scope("singleton")
+    @Bean("mainThreadPool")
+    public ExecutorService mainThreadPool() {
+        BlockingDeque<Runnable> working = new LinkedBlockingDeque<>(Integer.MAX_VALUE);
+        RejectedExecutionHandler handler = new ThreadPoolExecutor.CallerRunsPolicy();
+        ExecutorService threadPool = new ThreadPoolExecutor(2, Runtime.getRuntime().availableProcessors() + 1, 0L, TimeUnit.MICROSECONDS, working, handler);
+        return threadPool;
+    }
+
+    /**
+     * 此线程池用于IO密集型任务多线程调度
+     * IO密集型：核心线程数=2*CPU核心数(或 核心线程数=CPU核心数/(1-阻塞系数))
+     * 请直接使用，使用完毕后请勿关闭线程池
+     * @return
+     */
+    @Scope("singleton")
+    @Bean("deputyThreadPool")
+    public ExecutorService deputyThreadPool() {
+        BlockingDeque<Runnable> working = new LinkedBlockingDeque<>(Integer.MAX_VALUE);
+        RejectedExecutionHandler handler = new ThreadPoolExecutor.CallerRunsPolicy();
+        ExecutorService threadPool = new ThreadPoolExecutor(2, Runtime.getRuntime().availableProcessors() << 1, 0L, TimeUnit.MICROSECONDS, working, handler);
+        return threadPool;
+    }
+
+    /**
+     * 此线程池用于混合型型任务多线程调度
+     * 混合型：核心线程数=(线程等待时间/线程CPU时间+1)*CPU核心数
+     * 请直接使用，使用完毕后请勿关闭线程池
+     * @return
+     */
+    @Scope("singleton")
+    @Bean("otherThreadPool")
+    public ExecutorService otherThreadPool() {
+        BlockingDeque<Runnable> working = new LinkedBlockingDeque<>(Integer.MAX_VALUE);
+        RejectedExecutionHandler handler = new ThreadPoolExecutor.CallerRunsPolicy();
+        ExecutorService threadPool = new ThreadPoolExecutor(5, 20, 0L, TimeUnit.MICROSECONDS, working, handler);
+        return threadPool;
+    }
+}
