1e49309351324bbfc07c0546171387827e63f5df
KervinLi <614957650@qq.com>
2022-04-19 21:57:24 +0800

    自定义线程池测试用例

--- src/test/java/com/tool/thread/ThreadTest.java
+++ src/test/java/com/tool/thread/ThreadTest.java
@@ -1,11 +1,13 @@
 package com.tool.thread;
 
 import com.tool.thread.forkjoin.MyForkJoinTask;
+import com.tool.thread.pool.MyThreadPool;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ForkJoinTask;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.concurrent.*;
 
 /**
  * @Description: 线程相关测试
@@ -25,5 +27,38 @@ public class ThreadTest {
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    void testMyThreadPool(){
+        MyThreadPool myThreadPool = new MyThreadPool(3, 3, 0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>());
+        List<MyTask> taskList = new ArrayList<>();
+        for(int i=0;i<30;i++){
+            taskList.add(new MyTask());
+        }
+
+        try {
+            List<Future<Long>> futures = myThreadPool.invokeAll(taskList);
+            StringJoiner joiner = new StringJoiner("-","[","]");
+            for (Future<Long> f:futures){
+                joiner.add(String.valueOf(f.get()));
+            }
+            System.out.println("任务执行时间："+ joiner.toString());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            myThreadPool.shutdown();
+        }
+    }
+}
+//模拟任务 实现Callable接口
+class MyTask implements Callable<Long> {
+
+    @Override
+    public Long call() throws Exception {
+        long time= Math.round(Math.random() * 1000);
+        Thread.sleep(time);
+        return time;
     }
 }
